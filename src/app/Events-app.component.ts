51e54dda9b88c41f5dfa7a448c4030e32de70075
bhagavan.basvani@capgemini.com <bhagavan.basvani@capgemini.com>
2020-03-28 22:10:23 +0530

    appliying styles to DOM

--- src/app/Events-app.component.ts
+++ src/app/Events-app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   `
 })
 export class EventsAppComponent {
-  title = 'Hello World';
+  title = 'Hello Kanna';
 }
--- src/app/events/event-list.component.ts
+++ src/app/events/event-list.component.ts
@@ -4,20 +4,22 @@ import {  Component} from "@angular/core";
     selector:'event-list',
     template:`
     <div>
-        <h1>Upcoming Angular Events!</h1>
+        <h1>Angular events!</h1>
         <hr/>
-        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <div class="row">
+            <div *ngFor ="let event of events" class="col-md-5" >
+                <event-thumbnail [event]="event"></event-thumbnail>
+            </div>
+        </div>
     </div>
     `
 })
 
 export class EventListComponent{
-    const: any 
-    event1 = {
-        name:'ngConf 2025', 
-        date: '3/1/2025', 
-        time: '8am',
-        price:100, 
-        location: {address: '123 Main St', city: 'Salt Lake City, UT', country: 'USA'}
-    }
+     events = [
+        {name:'Angular Connect', date: '9/26/2036', time: '10:00 am', location: {address: '1 London Road ', city: 'London', country: 'England'}, format:"InPerson"},
+        {name:'ng-conf 2037', date: '4/15/2037', time: '9:00 am', onlineUrl: 'https://www.ng-conf.org/', format:"Online"},
+        {name:'Future Conf (Location/Url TBD)', date: '6/10/2037', time: '8:00 am'},
+        {name:'UN Angular Summit', date: '6/10/2037', time: '8:00 am', location: {address: 'The UN Angular Center', city: 'New York', country: 'USA'}, format:"InPerson"}
+      ]
 };
